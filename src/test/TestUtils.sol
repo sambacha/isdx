a6c9572dc749ca4d7ed8366a14324297b1f31c29
sam bacha <sam@manifoldfinance.com>
2022-08-02 22:55:43 -0700

    TestUtils

---
+++ src/test/TestUtils.sol
@@ -0,0 +1,40 @@
+// SPDX-License-identifier: MPL-2.0
+pragma solidity ^0.8.4;
+
+/// @title TestUtils
+contract TestUtils {
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+    function bytesToBytes32(bytes memory b, uint offset) private pure returns (bytes32) {
+        bytes32 out;
+
+        for (uint i = 0; i < 32; i++) {
+            out |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+        }
+        return out;
+    }
+
+    function min(uint x, uint y) internal returns (uint) {
+        if (x < y) return x;
+        return y;
+    }
+}
